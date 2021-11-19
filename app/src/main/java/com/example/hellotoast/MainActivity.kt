1731110cb3863a44c930281068349fbf21cf34c3
raultilves <raulsantanatilves7102gmail.com>
2021-11-19 12:25:21 +0000

    Cambios funcionales

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var mCount: Int = 0
+    lateinit var mShowCount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
     }
 
     fun showToast(view: android.view.View) {
         Toast.makeText(this, "Hello Toast", Toast.LENGTH_LONG).show()
     }
-    fun countUp(view: android.view.View) {}
-    fun reset(view: android.view.View) {}
+    fun countUp(view: android.view.View) {
+        mCount++
+        mShowCount.text = mCount.toString()
+    }
+    fun reset(view: android.view.View) {
+        mCount = 0
+        mShowCount.text = mCount.toString()
+    }
 }
